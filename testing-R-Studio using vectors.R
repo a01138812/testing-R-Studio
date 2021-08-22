52f9dcea6767079da095b6892b0e066d15d1a0ba
a01138812 <89359810+a01138812@users.noreply.github.com>
2021-08-22 16:38:27 -0500

    Add files via upload

---
+++ testing-R-Studio using vectors.R
@@ -0,0 +1,7 @@
+Codes for creating vectors using c()function()
+  
+z<-c(1, 2)           # numeric
+z<c(TRUE, FALSE)     # logical
+z<-c("x", "y", "z")  # character  
+z<-1:10              # integer
+z<-c(1+oi, 2_1i)     # complex 
